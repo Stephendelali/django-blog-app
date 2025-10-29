d60d273f13728985bb806040a6a9de762b877dce
Stephenamankwa <dsamankwa@st.ug.ed.gh>
2025-10-29 16:08:35 +0000

    feat: add image display to post detail page

--- blog/models.py
+++ blog/models.py
@@ -2,20 +2,33 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from PIL import Image
+
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='post_pics', default='default_post.jpg', blank=True)
 
     def __str__(self):
         return self.title
     
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk': self.pk})
-
     
-    
-
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        
+        if self.image:
+            try:
+                img = Image.open(self.image.path)
+                if img.height > 800 or img.width > 800:
+                    output_size = (800, 800)
+                    img.thumbnail(output_size)
+                    img.save(self.image.path)
+            except Exception:
+                # Prevent crash if the image file doesn't exist yet
+                pass
 
